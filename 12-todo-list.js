12d42254251997480fb51482685450cecc19da0c
Johnboscooo <124047293+Johnboscooo@users.noreply.github.com>
2023-06-07 14:19:08 +0100

    Add files via upload

---
+++ 12-todo-list.js
@@ -0,0 +1,58 @@
+const todoList = [{
+  name: 'Shedule Meeting',
+  dueDate: '2023-10-23'
+}, {
+  name: 'Order On Chart',
+  dueDate: '2023-06-23'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button> 
+    `;
+    todoListHTML += html;
+  });
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
+}
+
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addTodo();
+  });
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate,
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
